73393da0f06fd40d8ac4e74eb78b187a0e925cca
Szabolcs778 <97szabi@gmail.com>
2024-11-04 19:55:33 +0100

    commit

--- szoftverleltar/src/main/java/org/example/szoftverleltar/UserRepository.java
+++ szoftverleltar/src/main/java/org/example/szoftverleltar/UserRepository.java
@@ -4,5 +4,5 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends CrudRepository<User, Integer>
 {
-    Optional<User> findByEmail(String email); // email alapján lesz a bejelentkezés
+    Optional<User> findByEmail(String email);
 }
